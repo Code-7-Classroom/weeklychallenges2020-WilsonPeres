a20ae890ccb36dc545975a86883c5f70170b9850
Wilson Peres <r2h@adminisatorsmbp.home>
2020-04-13 17:04:47 -0400

    Uploading file

---
+++ checkPointWeek10/checkpoint.js
@@ -0,0 +1,10 @@
+// 1) They hold information by having the 
+// ability to insert many diffrent datatypes and variables within the object. 
+
+var Me = {
+    Name: "Wilson Peres"
+    
+    };
+  console.log(ME.sayHi())
+
+  
